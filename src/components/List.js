6420e485aa67044e5f9ec1de8097120292630c67
Vincent-Job <vinjob30@gmail.com>
2019-12-27 16:07:12 +0000

    hote-app undate

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NoteCard from './NoteCard';
 
-class List extends React.Component {
+class List extends Component {
+
   componentWillMount() {
-  	this.props.getNotes();
+    this.props.getNotes();
   }
 
   render() {
-  	return (
-  	   <div className="list-container">
-        List Component
-      </div>	
-	);
+  	const { notes, getNote } = this.props;
+
+  	const cards = notes.map((note, index) => {
+      return (
+        <NoteCard
+          key={index}
+          index={index}
+          note={note}
+          getNote={getNote}
+        />
+      );
+    });
+
+    return (
+      <div className="list-container">
+        {cards}
+      </div>
+    );
   }
 }
 
---
+++ src/components/NoteCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class NoteCard extends Component {
+  render() {
+    const { note, getNote } = this.props;
+
+    return (
+      <div className="note-card-container">
+        <div className="note-card-title">
+          {note.title}
+        </div>
+        <div className="note-card-content">
+          {note.content}
+        </div>
+        <span className="note-card-delete">
+          <i className="material-icons">close</i>
+        </span>
+        <span className="note-card-edit" onClick={() => getNote()}>
+          <i className="material-icons">mode_edit</i>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default NoteCard;
